9abdd4121070c8ba80d029876ed320ea206cef2e
Laura van den Brink <s1182915@student.windesheim.nl>
2022-09-07 13:16:15 +0200

    First Lesson

---
+++ Week 1/main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main(void) {
+	
+	int a;
+	int b;
+	int c;
+
+	a = 10;
+
+	printf("gimme a number, any number...");
+	scanf_s("%d", &b); 
+	if (b == 69) {
+		printf("you dirty pig \n");
+	} 
+	if (b == 420) {
+		printf("B) \n");
+	}
+
+	else {
+
+		printf("boring");
+
+	}
+
+	c = a * b;
+
+	printf("be gay do crime %d \n", a + b);
+	printf("A keer B is: %d", c);
+
+	return 0;
+}
